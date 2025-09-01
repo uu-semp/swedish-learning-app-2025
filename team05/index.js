b40946ab649eb71af4a6af1326b96be94ca9846f
xFrednet <xFrednet@gmail.com>
2025-09-01 11:48:37 +0200

    Add team02..20

---
+++ team05/index.js
@@ -0,0 +1,26 @@
+// ==============================================
+// Owned by Team 05
+// ==============================================
+
+"use strict";
+
+$(function() {window.vocabulary.when_ready(function () {
+
+  // These are only dummy functions and can be removed.
+  $("#check-jquery").on("click", () => {
+    alert("JavaScript and jQuery are working.");
+  });
+
+  $("#display-vocab").text(JSON.stringify(window.vocabulary.get_random()));
+
+  $("#check-saving").on("click", () => {
+    var data = window.save.get("team05");
+    data.counter = data.counter ?? 0;
+    data.counter += 1;
+    $("#check-saving").text(`This button has been pressed ${data.counter} times`);
+    window.save.set("team05", data);
+  });
+
+})});
+
+
