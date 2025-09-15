84b6cb67e6c374f50e47610af29997bd79f83987
Fridtjof Stoldt <xFrednet@gmail.com>
2025-09-15 17:37:41 +0200

    Data: Update `vocabulary.js` for the new vocab format

--- scripts/vocabulary.js
+++ scripts/vocabulary.js
@@ -6,6 +6,13 @@
 
 "use strict";
 
+window._vocabulary = {
+    vocab: null,
+    categories: null,
+    pending: 0
+}
+
+window._vocabulary.pending += 1;
 // This request is async
 // The .. is relative from a team folder. This is needed to support localhost
 // and GH pages
@@ -13,19 +20,59 @@ fetch("../assets/vocabulary.json")
     .then(res => res.json())
     .then(data => {
         console.log("Successfully loaded the vocabulary")
-        window._vocabulary = data;
+        window._vocabulary.vocab = data;
 
-        runCallbacks();
+        window._vocabulary.pending -= 1;
+        checkCallbacks();
     })
-    .catch(err => console.error("Failed to load JSON:", err));
+    .catch(err => console.error("Failed to load `vocabulary.json`", err));
+
+window._vocabulary.pending += 1;
+fetch("../assets/categories.json")
+    .then(res => res.json())
+    .then(data => {
+        console.log("Successfully loaded the categories")
+        window._vocabulary.categories = data;
+
+        window._vocabulary.pending -= 1;
+        checkCallbacks();
+    })
+    .catch(err => console.error("Failed to load `categories.json`", err));
+
 
 // Provide functions to be used by other scripts
 window.vocabulary = {
     callbacks: [],
 
+    // This returns the vocabulary metadata belonging to the given ID.
+    //
+    // Words usually have the following metadata:
+    // - `en`       The vocabulary in english
+    // - `sv`       The vocabulary in swedish
+    // - `article`  The article of the word (Optional)
+    // - `literal`  The literal representation of the word (Optional)
+    get_vocab(id) {
+        if (id in window._vocabulary.vocab) {
+            return window._vocabulary.vocab[id];
+        }
+
+        return null;
+    },
+
+    // Returns a list of vocabulary IDs belonging to the given category.
+    get_category(category) {
+        if (category in window._vocabulary.categories) {
+            return window._vocabulary.categories[category];
+        }
+
+        return null;
+    },
+
+    // Returns a random vocabulary item. See `get_vocab()`
     get_random() {
-        let idx = Math.floor(Math.random() * window._vocabulary.vocab.length);
-        return window._vocabulary.vocab[idx];
+        const ids = Object.keys(window._vocabulary.vocab);
+        const rand_id = ids[Math.floor(Math.random() * ids.length)];
+        return this.get_vocab(rand_id);
     },
 
     when_ready(callback) {
@@ -37,17 +84,23 @@ window.vocabulary = {
     }
 };
 
-function runCallbacks() {
+function checkCallbacks() {
+    // Check that `window.vocabulary` is already defined
     if (window.vocabulary === undefined) {
+        return;
+    }
+
+    // Check if all pending requests are done
+    if (window._vocabulary.pending > 0) {
         return;
     }
 
     // Snapshot the current callbacks
     const cbs = window.vocabulary.callbacks;
-    
+
     // Clear the list again
     window.vocabulary.callbacks = []
-    
+
     // Call all callbacks
     for (const cb of cbs) {
         if (typeof cb === "function") cb();
