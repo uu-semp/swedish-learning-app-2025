9693128a1447b913742895b13de052ba2f98dccc
Sebastian Blom <sebastian.blom.2227@student.uu.se>
2025-09-17 12:33:25 +0200

    stab at hifi-prototype

--- index.js
+++ index.js
@@ -5,7 +5,19 @@
 "use strict";
 
 $(function() {
-  $("#check-jquery").on("click", () => {
-    alert("JavaScript and jQuery are working.");
-  });
+    $("#check-jquery").on("click", () => {
+        alert("JavaScript and jQuery are working.");
+    });
 });
+
+function openIframe(gameUrl) {
+    const gameDisplay = document.querySelector(".game-display");
+    const iframe = gameDisplay.querySelector("iframe");
+
+    if (gameUrl) {
+        iframe.src = gameUrl;
+    }
+
+    // gameDisplay.style.display is by default set to none i.e it is invisable.
+    gameDisplay.style.display = "block";
+}
