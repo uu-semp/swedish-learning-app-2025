c27980c52ef44637678b14680ac63b425a6dae00
tagepennstrom <tagepennstrom@hotmail.com>
2025-09-22 23:50:47 +0200

    Fetch items function + css change

--- team16/index.js
+++ team16/index.js
@@ -1,11 +1,35 @@
 // ==============================================
-// Owned by Team 15
+// Owned by Team 11
 // ==============================================
 
 "use strict";
 
-$(function() {window.vocabulary.when_ready(function () {
+// Returns an array with all ids for this teams items.
+function getItemsIds() {
+  const Ids = [];
+  const foods = window.vocabulary.get_category("food");
+  const fruits = window.vocabulary.get_category("fruit");
+  foods.map(id => {
+    Ids.push(id);
+  })
+  fruits.map(id => {
+    Ids.push(id);
+  })
+  return Ids;
+}
 
+// Returns an array of Items.
+function getItems() {
+  const items = []
+  const ids = getItemsIds();
+  ids.map(id => {
+    items.push(window.vocabulary.get_vocab(id))
+  })
+  return items;
+
+}
+
+$(function() {window.vocabulary.when_ready(function () {
   // These are only dummy functions and can be removed.
   $("#check-jquery").on("click", () => {
     alert("JavaScript and jQuery are working.");
@@ -14,11 +38,17 @@ $(function() {window.vocabulary.when_ready(function () {
   $("#display-vocab").text(JSON.stringify(window.vocabulary.get_random()));
 
   $("#check-saving").on("click", () => {
-    var data = window.save.get("team15");
+    var data = window.save.get("team11");
     data.counter = data.counter ?? 0;
     data.counter += 1;
     $("#check-saving").text(`This button has been pressed ${data.counter} times`);
-    window.save.set("team15", data);
+    window.save.set("team11", data);
   });
 
-})});
+  // Load all team specific data
+  const items = getItems();
+  if (items != null) {
+    console.log("Fetch all items")
+  }
+
+});});
