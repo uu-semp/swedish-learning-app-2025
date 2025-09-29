e2d2491ef5bbbc514fec6699f44586cf16b5a145
Olivia Lundbergh <olivia.lundbergh.1235@student.uu.se>
2025-09-29 13:03:52 +0200

    Added level 1 button with functionality and related view

--- team15/components/index.js
+++ team15/components/index.js
@@ -1,11 +1,13 @@
 import { StartView } from "./views/startView.js";
 import { HelpView } from "./views/helpView.js";
 import { ChooseLevelView } from "./views/chooseLevelView.js";
+import { LevelOneView } from "./views/levelOneView.js";
 import {
   StartGameButton,
   HowToPlayButton,
   InfoButton,
   GoBackButton,
+  LevelButton
 } from "./elements/buttons.js";
 
 
@@ -13,8 +15,10 @@ export default {
   StartView,
   HelpView,
   ChooseLevelView,
+  LevelOneView,
   StartGameButton,
   HowToPlayButton,
   GoBackButton,
   InfoButton,
+  LevelButton
 };
