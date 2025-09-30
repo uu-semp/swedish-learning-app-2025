7fc82442232c51e649f6dfcc9c8d16b86926aad8
David Johnson <david_karl_johnson@hotmail.com>
2025-09-30 19:55:05 +0200

    Create documentation for 'setClock' JS function

--- team04/utils/setClock.js
+++ team04/utils/setClock.js
@@ -1,3 +1,10 @@
+/**
+ * Sets the rotation of the clock component's hour and minute hands based on the time arguments.
+ *
+ * @param {int} hour - The hour value (0-23)
+ * @param {int} minute - The minute value (0-59)
+ * @throws {Error} Throws an error if hour is not in [0, 24) or minute is not in [0, 60)
+ */
 export default function setClock(hour, minute) {
     if (hour < 0 || hour >= 24) {
         throw new Error('Wrong hour argument. Hour should be within [0, 24)');
