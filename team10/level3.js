fa7a5e6963a92cd5601ca0ae6bbd8e4e8b10e039
Henrik Langlott <henrik.langlott@gmail.com>
2025-10-01 20:12:21 +0200

    SCRUM-143 Feat: Added field for instructions

--- team10/level3.js
+++ team10/level3.js
@@ -15,10 +15,23 @@ const DisplayLevel = {
     }
 }
 
+const DisplayInstructions = {
+    template: `
+        <div class="display-instructions">
+            <p>{{ LevelInstructions }}</p>
+        </div>
+        `,
+    data() {
+        return {
+            LevelInstructions: 'Instructions: Given the image and the English word, write the Swedish word. Then press “Enter” to submit your response.'
+        }
+    }
+}
 
 const app = createApp({})
 
 app.component('display-level', DisplayLevel)
+app.component('display-instructions', DisplayInstructions)
 
 
 app.mount('#app')
