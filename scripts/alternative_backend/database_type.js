7472eabf0ecca5eb9de2771fa509a6ee1858cbcb
Valle <darkfrobozz.mega@gmail.com>
2025-10-01 19:13:29 +0200

    added alternative backend and limited interface

---
+++ scripts/alternative_backend/database_type.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {Object} VocabEntry
+ * @property {string} en - English word (e.g., "sweater").
+ * @property {string} sv - Swedish translation.
+ * @property {string} article - Swedish article ("en" or "ett").
+ * @property {string} img - Copyright or license information for the image.
+ * @property {string} img_copyright - URL or path to the image.
+ * @property {string} audio - URL or path to audio pronunciation (may be empty).
+ */
+
+export const DB = {};
