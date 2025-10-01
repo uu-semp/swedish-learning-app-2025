2ba5bcd3bad5cfc05bc1d0ebf33a2bbcf097f0aa
Valle <darkfrobozz.mega@gmail.com>
2025-10-01 19:13:29 +0200

    added alternative backend and limited interface

---
+++ scripts/alternative_backend/database_config.js
@@ -0,0 +1,8 @@
+export const FETCH_EXTERNAL = true;
+
+// Change this if you google sheets location has changed
+export const SHEETID = "1de16iRzmgSqWvTTxiNvQYM79sWJBwFJN0Up3Y0allDg";
+export const EXTERNAL_URL = `https://docs.google.com/spreadsheets/d/${SHEETID}/export?format=csv&gid=0`;
+
+// Change this if you have changed csv file
+export const INTERNAL_URL = "../../../words.csv";
