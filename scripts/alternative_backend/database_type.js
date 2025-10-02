d5c6339557dbce181a2ba307dc469c52cde9858e
Valle <darkfrobozz.mega@gmail.com>
2025-10-02 20:43:53 +0200

    fixed type error

--- scripts/alternative_backend/database_type.js
+++ scripts/alternative_backend/database_type.js
@@ -3,9 +3,9 @@
  * @property {string} en - English word (e.g., "sweater").
  * @property {string} sv - Swedish translation.
  * @property {string} article - Swedish article ("en" or "ett").
- * @property {string} img - Copyright or license information for the image.
- * @property {string} img_copyright - URL or path to the image.
- * @property {string} audio - URL or path to audio pronunciation (may be empty).
+ * @property {string} img - image url.
+ * @property {string} img_copyright - Copyright or license information for the image.
+ * @property {string} audio - URL or path to audio pronunciation.
  */
 
 export const DB = {};
