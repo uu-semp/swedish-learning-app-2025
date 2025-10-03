12e4edb9cd369d03595cea099671a0ce5fa8c274
abdurrehmankhalid <abdurehmank7@gmail.com>
2025-10-03 18:33:57 +0200

    we are now able to load images onto image view by changing item name, everything is dynamic

--- team15/components/views/imageView.js
+++ team15/components/views/imageView.js
@@ -1,16 +1,48 @@
+import clothingItems from "/team15/clothing-items-info.js";
+
 export const ImagesView = {
   name: "images-view",
   props: ["switchTo"], // Receives navigation method from parent
   
+  mounted () {
+    console.log(clothingItems);
+    console.log(clothingItems.length);
+  },
+
+  data() {
+    return {
+      itemName: "sneakers" // Local variable for item name (can be changed dynamically if needed)
+    };
+  },
+
+  // Compute the current item based on itemName
+  computed: {
+    currentItem() {
+      // Find the clothing item object that matches the itemName
+      return clothingItems.find(item => item.English === this.itemName);
+    }
+  },
+
   template: `
-    <div>
-      <!-- Blank page - add content here as needed -->
-
-      <!-- Hardcoded image for testing (using beanie from clothing-items-info.js) -->
+    <div class="main-container">
+      <!-- Dynamic image based on currentItem -->
       <div class="image-container">
-        <img src="../../../assets/images/clothes/beanie.png" alt="Beanie" class="displayed-image" /> </div>
+        <img v-if="currentItem" :src="\`../../../\${currentItem.Image_url}\`" :alt="\`\${currentItem.English}\`" class="displayed-image" />
+      </div>
 
-      <div class = button-container> 
+      <!-- Item Details -->
+      <div class="item-details" v-if="currentItem">
+        <p><strong>ID:</strong> {{ currentItem.ID }}</p>
+        <p><strong>English word:</strong> {{ currentItem.English }}</p>
+        <p><strong>Article:</strong> {{ currentItem.Article || 'N/A' }}</p>
+        <p><strong>Swedish word:</strong> {{ currentItem.Swedish }}</p>
+        <p><strong>Swedish plural:</strong> {{ currentItem.Swedish_plural || 'N/A' }}</p>
+        <p><strong>Category:</strong> {{ currentItem.Category }}</p>
+        <p><strong>Image URL:</strong> {{ currentItem.Image_url }}</p>
+      </div>
+
+      <!-- Back button -->
+      <div class="button-container"> 
         <go-back-button @click="switchTo('StartView')"></go-back-button>
       </div>
     </div>
