c47bbdb10afead93daebde4f1351d5a338dedbb4
majakadestam <maja@njorda.se>
2025-10-03 11:30:41 +0200

    [G15-114] Added pelle component

---
+++ team15/components/elements/pelleContainer.js
@@ -0,0 +1,11 @@
+export const PelleContainer = {
+  name: "pelle-container",
+  methods: {
+  },
+  template: `
+        <div class="drop-zone">
+        <slot> "Pelle Container" </slot>
+         </div>
+  `,
+};
+
