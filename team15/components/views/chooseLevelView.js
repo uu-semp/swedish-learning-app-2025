6b5755ae73981c43137651034466a1ffc0272414
Stanislaw Kaminski <stanek0909@gmail.com>
2025-10-04 14:17:24 +0200

    adding level three view and button

--- team15/components/views/chooseLevelView.js
+++ team15/components/views/chooseLevelView.js
@@ -6,6 +6,7 @@ export const ChooseLevelView = {
       <h1 class = "main-text">CHOOSE LEVEL</h1>    
         <level-button label="LEVEL 1" @click="switchTo('LevelOneView')"></level-button>
         <level-button label="LEVEL 2" @click="switchTo('LevelTwoView')"></level-button>
+        <level-button label="LEVEL 3" @click="switchTo('LevelThreeView')"></level-button>
 
       <div class = button-container> 
         <go-back-button @click="switchTo('StartView')"></go-back-button>
