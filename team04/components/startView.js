a60a6c1c16dfd8aae98f4169709daf4f97303207
ElysiaLiu <Elysialiu1230@gmail.com>
2025-10-04 23:30:02 +0200

    Add start page component (clean version)

--- team04/components/startView.js
+++ team04/components/startView.js
@@ -1,39 +1,32 @@
 export default {
   name: "startView",
   template: `
-    <div style="display: flex; flex-direction: column; align-items: center; padding: 40px; font-family: Arial, sans-serif;">
-
-      <!-- game title -->
-      <h1 style="margin-bottom: 20px; font-size: 32px; font-weight: bold; color: #222;">
-        Clock it!
-      </h1>
-
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;"> 
       <!-- game instruction -->
-      <div style="background: #fff3cd; color: #5a4636; padding: 20px 30px; border-radius: 12px; box-shadow: 0 4px 10px rgba(0,0,0,0.1); max-width: 600px; margin-bottom: 30px;">
+      <div style="background: #fff3cd; color: #5a4636; padding: 20px 20px; border-radius: 12px; box-shadow: 0 4px 10px rgba(0,0,0,0.1); max-width: 600px; margin-bottom: 30px;">
         <h3 style="margin-bottom: 10px; font-weight: bold; color: #856404;">Game Instruction</h3>
         <ol style="margin: 0; padding-left: 20px; line-height: 1.6;">
-          <li>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
-          <li>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
-          <li>xxxxxxxxxxxxxxxxx</li>
+          <li>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<>
+          <li>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<>
+          <li>xxxxxxxxxxxxxxxxx<>
         </ol>
       </div>
 
       <!-- buttons guide to each level-->
-      <div style="display: flex; flex-direction: column; gap: 15px; width: 200px;">
+      <div style="display: flex; flex-direction: column; gap: 15px; width: 30%;">
         <button @click="$root.currentView = 'game'" 
                 style="padding: 12px; border: none; border-radius: 8px; background: #5de9acc8; color: white; font-size: 18px; cursor: pointer;">
           Level 1
-        </button>
+        <tton>
         <button @click="$root.currentView = 'game'" 
                 style="padding: 12px; border: none; border-radius: 8px; background: #3786ed86; color: white; font-size: 18px; cursor: pointer;">
           Level 2
-        </button>
+        <tton>
         <button @click="$root.currentView = 'game'" 
                 style="padding: 12px; border: none; border-radius: 8px; background: #ef4a5ab3; color: white; font-size: 18px; cursor: pointer;">
           Level 3
-        </button>
+        <tton>
       </div>
-
     </div>
   `,
   data() {
