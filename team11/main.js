e42545f13259cf969f5c71330bdcc5a86aab8840
tagepennstrom <tagepennstrom@hotmail.com>
2025-10-06 12:21:24 +0200

    Revert "bugg i globala assets så måste rebasea"
    This reverts commit b5c9f292a276575a455b08b4036eaa1e1963cf54.

--- team11/main.js
+++
@@ -1,9 +0,0 @@
-import { initGameState } from "./state.js";
-
-let gameState = {};
-
-$(function() {
-    console.log("main.js körs");
-    gameState = initGameState();
-    console.log("What in the world its working!")
-})
