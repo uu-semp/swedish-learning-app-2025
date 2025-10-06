644aec8aa2d0e4d7e24b8591efef75f21d96cce4
simongebreyohannes <simongebreyohannes99@gmail.com>
2025-10-06 20:25:19 +0200

    added some hardcoded questions for the moment. they are triggered by the drag-and-drop events. need to extend to keep check when users do something right/wrong

---
+++ team02/scripts/level1Questions.js
@@ -0,0 +1,7 @@
+const level1Questions = [
+  { question: "Flytta 'bord' till mitten av arbetsytan.", answer: "ett.png" },
+  { question: "Placera 'bänk' till vänster om 'bord'.", answer: "bänk" },
+  { question: "Ställ 'byrå' längst upp till höger.", answer: "byrå" },
+  { question: "Flytta 'arbetsbord' nära botten av arbetsytan.", answer: "arbetsbord" }
+  // Lägg till fler frågor vid behov
+];
