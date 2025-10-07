f40577bc0e60ddffc47b61be1cc13e65eeba1677
Henrik Langlott <henrik.langlott@gmail.com>
2025-10-07 09:50:49 +0200

    Team 10 - 138 Refactored to use vue components instead of separate html pages. Added script that fetches 10 random words when starting a game.

--- team10/level_three/game_page.js
+++ team10/level_three/game_page.js
@@ -4,38 +4,56 @@
 
 const { createApp } = Vue
 
+const BackButton = {
+    template: `
+    <button class="back-btn" @click="$emit('back')">
+        <i class="fa fa-arrow-left"></i>
+    </button>
+    `
+};
+
+const MenuPage = {
+    components: {BackButton},
+    template: `      
+    <div class="container">
+        <h1 class="title">EatAndLearn</h1>
+
+        <p class="instructions">Instructions: Write the Swedish word for the food item in the image.</p>
+        <div class="button-container">
+            <back-button @back="goBack"></back-button>
+            <button class="start-btn" @click="$emit('start')">Start</button>
+        </div>
+        <div class="progress-container">
+            <div class="progress-icon">
+                <div class="progress-bar">
+                    <div class="progress"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `,
+    methods: {
+        goBack() {
+            window.location.href = '../index.html'
+        }
+    }  
+};
+
 const DisplayLevel = {
     template: `
     <h2 class="display-level">{{ LevelText }}</h2>
     `,
     data() {
         return {
-            LevelText: 'Level 3'
+            LevelText: 'Level 3 - Spelling'
         }
     }
 }
 
-const DisplayInstructions = {
-    template: `
-        <div class="display-instructions">
-            <p>{{ LevelInstructions }}</p>
-        </div>
-        `,
-    data() {
-        return {
-            LevelInstructions: 'Instructions: Given the image and the English word, write the Swedish word. Then press “Enter” to submit your response.'
-        }
-    }
-}
-
-const DispayImage = {
-    template: `
-    <img :src="imgSrc" alt="Alternative image text" class=display-image>
-    `,
-    data() { 
-        return {imgSrc: "../../assets/images/food/food.png"}
-    }
-}
+const DisplayImage = {
+    props: ['imgSrc'],
+    template: `<img :src="imgSrc" alt="Alternative image text" class="display-image">`
+};
 
 const AnswerContainer = {
     template: `
@@ -44,38 +62,77 @@ const AnswerContainer = {
         <button type="submit" class="submit-button">Submit</button>
     </form>
     `,
+    data() {
+        return { answer: ''}
+    },
+    emits: ['submit'], 
     methods: {
         submitAnswer() {
             console.log('submitAnswer called, answer=', this.answer)
-            alert('Your answer: ' + this.answer)
+            this.$emit('submit', this.answer)
             this.answer = ''
         }
-    },
-    data() {
-        return { answer: ''}
     }
-}
+};
 
-const BackToMenu = {
+const GamePage = {
     template: `
-    <button @click="goBack" class="back-btn">
-        <i class="fa-solid fa-arrow-left"></i> Back
-    </button>
+    <div class="game">
+        <back-button @back="goBack"></back-button>
+        <display-level></display-level>
+        <display-image :imgSrc="currentImg"></display-image>
+        <p class="english-word">What is the Swedish word for: <strong>{{ currentWord?.en || 'Loading...' }}</strong>?</p>
+        <answer-container @submit="handleSubmit"></answer-container>
+    </div>
     `,
+    components: { DisplayLevel, DisplayImage, AnswerContainer, BackButton },
+    data() {
+        return {
+            words: [], 
+            currentIndex: 0, 
+            defaultImg: "assets/images/food/food.png"
+        };
+    },
+    computed: {
+        currentWord() {
+            return this.words[this.currentIndex] || null;
+        },
+        currentImg() {
+            return "../../" + this.currentWord?.img || this.defaultImg;
+        }
+    },
+    async mounted() {
+        if (!window.game_logic) {
+            console.error("game_logic is not loaded");
+            return;
+        }
+        this.words = await window.game_logic.getRandomWordSet();
+        console.log('Words loaded:', this.words);
+    },
     methods: {
+        handleSubmit(answer) {
+            console.log('handleSubmit called with answer:', answer);
+            alert(`You submitted: ${answer}`);
+            this.nextWord();
+        },
+        nextWord() {
+            this.currentIndex = (this.currentIndex + 1) % this.words.length;
+            console.log('nextWord called, next index:', this.currentIndex);
+        },
         goBack() {
-            window.location.href = 'level_three.html'
+            this.currentView = 'menu';
         }
     }
-}
+};
 
 
-const app = createApp({})
-
-app.component('display-level', DisplayLevel)
-app.component('display-instructions', DisplayInstructions)
-app.component('display-image', DispayImage)
-app.component('answer-container', AnswerContainer)
-app.component('back-to-menu', BackToMenu)
-
-app.mount('#app')
+const app = createApp({
+    components: { MenuPage, GamePage, BackButton},
+    data() {
+        return { currentView: 'menu' };
+    },
+    template: `
+    <menu-page v-if="currentView === 'menu'" @start="currentView='game'"></menu-page>
+    <game-page v-else></game-page>
+    `,
+}).mount('#app');
