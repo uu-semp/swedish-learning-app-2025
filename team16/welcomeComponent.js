8d7fbb9337c9fa38de097b46594aef94697511a8
Harikrishnan Vinod <harikrishnan1@hotmail.com>
2025-10-07 12:30:36 +0200

    team16/frontend 1st version complete

--- team16/welcomeComponent.js
+++ team16/welcomeComponent.js
@@ -11,7 +11,7 @@ let gameProgress = {
     level2: { 
         completed: 0, 
         total: 10, 
-        unlocked: false, 
+        unlocked: true, 
         attempts: 0,
         timeSpent: 0,
         lastPlayed: null
@@ -19,7 +19,7 @@ let gameProgress = {
     level3: { 
         completed: 0, 
         total: 10, 
-        unlocked: false, 
+        unlocked: true, 
         attempts: 0,
         timeSpent: 0,
         lastPlayed: null
