28863b7048ff2bfb4e3cb9c325fa4a84ada732dc
abdurrehmankhalid <abdurehmank7@gmail.com>
2025-10-07 17:24:07 +0200

    added styles for choose level view so it feels more consistent and is in line with our figma design

--- team15/components/views/chooseLevelView.js
+++ team15/components/views/chooseLevelView.js
@@ -2,15 +2,22 @@ export const ChooseLevelView = {
   name: "choose-level-view",
   props: ["switchTo"],
   template: `
-      <div>
-      <h1 class = "main-text">CHOOSE LEVEL</h1>    
-        <level-button label="LEVEL 1" @click="switchTo('LevelOneView')"></level-button>
-        <level-button label="LEVEL 2" @click="switchTo('LevelTwoView')"></level-button>
-        <level-button label="LEVEL 3" @click="switchTo('LevelThreeView')"></level-button>
+      <div class="choose-level-view">
+          
+          <div class="language-flags-container">
+              </div>
 
-      <div class = button-container> 
-        <go-back-button @click="switchTo('StartView')"></go-back-button>
-      </div>
+          <h1 class="main-text">CHOOSE LEVEL</h1>    
+          
+          <div class="level-buttons-container">
+              <level-button label="LEVEL 1" class="big-buttons" @click="switchTo('LevelOneView')"></level-button>
+              <level-button label="LEVEL 2" class="big-buttons" @click="switchTo('LevelTwoView')"></level-button>
+              <level-button label="LEVEL 3" class="big-buttons" @click="switchTo('LevelThreeView')"></level-button>
+          </div>
+
+          <div class="go-back-wrapper"> 
+              <go-back-button class="go-back-button-style" @click="switchTo('StartView')"></go-back-button>
+          </div>
       </div>
     `,
 };
