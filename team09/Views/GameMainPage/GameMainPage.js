1749d1a21c5b2e256276641876b192c5d6dc46aa
lizh42 <zonelottre@gmail.com>
2025-10-08 02:22:39 +0200

    Add information page and update GameMainPage logic

--- team09/Views/GameMainPage/GameMainPage.js
+++ team09/Views/GameMainPage/GameMainPage.js
@@ -2,4 +2,11 @@ function startGame() {
     
     window.location.href = "../levelSelectPage/levelSelect.html";
   }
-  
+
+function openDictionary() {
+    window.location.href = "../DictionaryPage/DictionaryPage.html";
+}
+
+function openInfo() {
+    window.location.href = "information.html";
+}
