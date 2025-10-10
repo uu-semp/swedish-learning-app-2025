c0915a0938553d1dff654913e6d0a4780a46b44e
Olivia Lundbergh <olivia.lundbergh.1235@student.uu.se>
2025-10-10 14:44:49 +0200

    styling edits

--- team15/components/elements/dressPellePrompt.js
+++ team15/components/elements/dressPellePrompt.js
@@ -3,8 +3,8 @@ export const DressPellePrompt = {
     props: ['item'],
 
     template: `
-    <div>
-      <h1 v-if="item">
+    <div class="dress-prompt-container">
+      <h1 class="question-prompt" v-if="item">
         {{$language.translate('dress-pelle-prompt')}} {{ item.Article }} <span v-if="item.is_pair">par </span> {{ item.Adjective_sv }} {{ item.Swedish }}
       </h1>
     </div>
