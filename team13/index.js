e0858f0a0ef19c6146743f5cfdc2fc85aa1df108
Ludde <luddemag@gmail.com>
2025-10-10 03:11:40 +0200

    Small fix to html file. Have converted the index.js file to use vue by working with the HTML and using the old functions as template everything has been converted to vue. There are still some minor bug which have to be fixed for it to run as intended

--- team13/index.js
+++ team13/index.js
@@ -5,147 +5,147 @@
 
 "use strict";
 
-function irandom_range(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const app = Vue.createApp({
+  data() {
+    return {
+      currentScreen: 'menu',
 
-const gameStates = {
-  menu: 0,
-  inGame: 1,
-};
+      progress: 1,
+      progressMax: 10,
 
-let state = gameStates.inGame;
+      houseOptions: [],
+      correctHouseIndex: -1,
+      currentQuestion: null,
+      currentStreet: '',
 
-let houseArray = [];
-let correctHouse = null;
-let houseCount = 4
+      showTranslation: false,
+      swedishSentence: ["Jag", "bor", "på", "-street", "-int"],
+      englishSentence: ["I", "live", "on", "-street", "-int"],
+      prompt: [],
+      translatedIndexes: [],
 
-$(function () {
-  window.vocabulary.when_ready(function () {
-    const houseInfo = runGameStateLogic();
+      vocabNumbers: [],
+      vocabStreets: [],
+    }
+  },
 
-    houseArray = houseInfo.houseArray;
-    correctHouse = houseInfo.correctHouse;
+  computed: {
+    progressPercentage() {
+      return (this.progress / this.progressMax) * 100 + '%';
+    },
+    translationButtonText() {
+      return this.showTranslation ? 'Hide translation' : 'Show translation';
+    }
+  },
 
-    console.log("Generated houses:", houseArray);
-    console.log("correct house index:", correctHouse);
+  methods: {
+    startGame() {
+      this.progress = 1;
+      this.currentScreen = 'game';
+      this.startNewRound();
+    },
+    backToMenu() {
+      this.currentScreen = 'menu';
+    },
+    startNewRound() {
+      this.translatedIndexes = [];
+      this.prompt = [...this.swedishSentence];
+      this.showTranslation = false;
 
-    $("#check-jquery").on("click", () => {
-      alert("JavaScript and jQuery are working.");
+      const randomNoIndex = this.irandom_range(1, this.vocabNumbers.length - 1);
+      const vocab = window.vocabulary.get_vocab(this.vocabNumbers[randomNoIndex]);
+      this.currentQuestion = vocab;
+
+      const randomStreetIndex = this.irandom_range(0, this.vocabStreets.length - 1);
+      this.currentStreet = window.vocabulary.get_vocab(this.vocabStreets[randomStreetIndex]).sv;
+
+      const houseCount = 4;
+      const highestNumber = this.vocabNumbers.length - 1;
+      const result = this.generateRandomHouses(vocab.literal, houseCount, highestNumber);
+
+      this.houseOptions = result.houseArray;
+      this.correctHouseIndex = result.correctHouse;
+    },
+
+    checkAnswer(selectedIndex) {
+      const wasCorrect = (selectedIndex === this.correctHouseIndex);
+      alert(wasCorrect ? "Correct!" : "Wrong house.");
+
+      this.progress += wasCorrect ? 1 : -1;
+      if (this.progress < 1)  {
+        this.progress = 1;
+      }
+
+      if (this.progress >= this.progressMax) {
+        alert("Congrats! You finished 10 rounds.");
+        this.backToMenu();
+        return;
+      }
+
+      this.startNewRound();
+    },
+
+    toggleTranslation() {
+      this.showTranslation = !this.showTranslation;
+    },
+
+    translateWord(wordIndex) {
+      if (this.translatedIndexes.includes(wordIndex)) return;
+      
+      this.translatedIndexes.push(wordIndex);
+      const englishWord = this.englishSentence[wordIndex];
+      const newPrompt = [...this.prompt];
+      newPrompt[wordIndex] = englishWord;
+      this.prompt = newPrompt;
+    },
+
+    renderWord(word) {
+      switch (word) {
+        case 
+          "-street": return this.currentStreet;
+
+        case 
+          "-int": return this.currentQuestion ? this.currentQuestion.sv : '';
+      
+        default: 
+          return word;
+      }
+    },
+    
+    generateRandomHouses(houseNumber, houseCount, highestNumber) {
+      const doubleHouses = this.irandom_range(0, 1);
+      const maxPos = Math.min(Math.floor((houseNumber - 1) / (1 + doubleHouses)), houseCount - 1);
+      const minPos = Math.min(
+        Math.max(Math.ceil((houseCount - 1) - (highestNumber - houseNumber) / (1 + doubleHouses)), 0),
+        houseCount - 1
+      );
+    
+      const relativeHousePosition = this.irandom_range(Math.max(0, minPos), maxPos);
+      const houses = [];
+    
+      for (let i = 0; i < houseCount; i++) {
+        houses.push(houseNumber - (relativeHousePosition - i) * (1 + doubleHouses));
+      }
+    
+      return { houseArray: houses, correctHouse: relativeHousePosition };
+    },
+
+    irandom_range(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+  },
+  
+  mounted() {
+    window.vocabulary.when_ready(() => {
+      console.log("Vocabulary loaded, vue ready");
+      this.vocabNumbers = window.vocabulary.get_category("number");
+      this.vocabStreets = window.vocabulary.get_category("street");
     });
-
-    $("#check-saving").on("click", () => {
-      var data = window.save.get("team13");
-      data.counter = data.counter ?? 0;
-      data.counter += 1;
-      $("#check-saving").text(`This button has been pressed ${data.counter} times`);
-      window.save.set("team13", data);
-    });
-  });
+  }
+  
 });
 
-function menu_logic() {
-  console.log("menu_logic");
-  return generateRandom();
-}
-
-function inGame_logic() {
-  console.log("inGame_logic");
-  return generateRandom();
-}
-
-function generateRandom() {
-  const numbers = window.vocabulary.get_category("number");
-  //vocabulary is NOT guaranteed to be in order. dont assume randomNo == literal
-  const randomNo = irandom_range(1, numbers.length - 1);
-
-  const vocab = window.vocabulary.get_vocab(numbers[randomNo]);
-  console.log("generateRandom:", vocab.literal);
-
-  $("#number-english").text((vocab.en));
-  $("#number-swedish").text((vocab.sv));
-  $("#number-literal").text((vocab.literal));
-
-  const result = generateRandomHouses(vocab.literal, houseCount, numbers.length - 1);
-
-  houseArray = result.houseArray;
-  correctHouse = result.correctHouse;
-  updateHouseButtons();
-  renderHouseButtons();
-
-  return result;
-}
-function renderHouseButtons() {
-  const container = $("#house-buttons");
-  container.empty(); // clear previous buttons
-
-  houseArray.forEach((houseNum, index) => {
-    const btn = $(`<button>House ${houseNum}</button>`);
-    btn.on("click", () => clickHouse(index));
-    container.append(btn);
-  });
-}
-
-function generateRandomHouses(houseNumber, houseCount, highestNumber) {
-  const doubleHouses = irandom_range(0, 1);
-  const maxPos = Math.min(Math.floor((houseNumber - 1) / (1 + doubleHouses)), houseCount - 1);
-  const minPos = Math.min(
-    Math.max(Math.ceil((houseCount - 1) - (highestNumber - houseNumber) / (1 + doubleHouses)), 0),
-    houseCount - 1
-  );
-
-  const relativeHousePosition = irandom_range(Math.max(0, minPos), maxPos);
-  const houses = [];
-
-  for (let i = 0; i < houseCount; i++) {
-    houses.push(houseNumber - (relativeHousePosition - i) * (1 + doubleHouses));
-  }
-
-  return { houseArray: houses, correctHouse: relativeHousePosition };
-}
-
-function nextGameState() {
-  const totalStates = Object.keys(gameStates).length;
-  state = (state + 1) % totalStates;
-  console.log("New state:", state);
-  runGameStateLogic();
-}
-
-function clickHouse(num) {
-  console.log("Clicked house:", num);
-  console.log("House array:", houseArray);
-  console.log("Correct house index:", correctHouse);
-
-  if (num === correctHouse) {
-    alert("Correct!");
-  } else {
-    alert("Wrong house.");
-  }
-}
-
-function updateHouseButtons() {
-  for (let i = 0; i < houseArray.length; i++) {
-    const button = document.getElementById(`house-btn-${i}`);
-    if (button) {
-      button.textContent = `House ${houseArray[i]}`;
-    }
-  }
-}
-
-function runGameStateLogic() {
-  const numbers = window.vocabulary.get_category("number");
-  $("#gamestate").text(window.vocabulary.get_vocab(numbers[state]).en);
-
-  switch (state) {
-    case gameStates.menu:
-      return menu_logic();
-    case gameStates.inGame:
-      return inGame_logic();
-  }
-}
-
-function getKeyName(states, index) {
-  return Object.keys(states).find((key) => states[key] === index);
-}
+app.mount('#app');
