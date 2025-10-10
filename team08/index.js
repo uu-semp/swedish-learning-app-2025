928627298381bc719473365d5eca42ac99bce095
Valle <darkfrobozz.mega@gmail.com>
2025-10-10 13:47:51 +0200

    added a fetch team function and tests for it

--- team08/index.js
+++ team08/index.js
@@ -1,26 +1,11 @@
+import {
+  loaddb,
+  test,
+} from "../scripts/alternative_backend/vocabulary_await.js";
 // ==============================================
 // Owned by Team 08
 // ==============================================
 
-"use strict";
+await loaddb();
 
-$(function() {window.vocabulary.when_ready(function () {
-
-  // These are only dummy functions and can be removed.
-  $("#check-jquery").on("click", () => {
-    alert("JavaScript and jQuery are working.");
-  });
-
-  $("#display-vocab").text(JSON.stringify(window.vocabulary.get_random()));
-
-  $("#check-saving").on("click", () => {
-    var data = window.save.get("team08");
-    data.counter = data.counter ?? 0;
-    data.counter += 1;
-    $("#check-saving").text(`This button has been pressed ${data.counter} times`);
-    window.save.set("team08", data);
-  });
-
-})});
-
-
+test();
