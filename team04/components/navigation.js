c65d4b4a3fdfef43171e9e76b953f31bb134ea2b
David Johnson <david_karl_johnson@hotmail.com>
2025-10-13 17:38:39 +0200

    Team04#101: Organize all nav buttons in a row
    All navigation buttons are now grouped together at the bottom, in a row,
    of the game frame. All buttons are equal in size.

--- team04/components/navigation.js
+++ team04/components/navigation.js
@@ -2,13 +2,10 @@ export default {
     name: "Navigation",
     template: `
       <nav class="navigation">
-        <div class="navigation-btn-group">
-            <button class="main-menu-btn">Main menu</button>
-            <button class="reset-btn">Reset level</button>
-        </div>
-        <div class="level-select-group">
-            <button class="level-btn" @click='$emit("prev")'>&#8678;</button>
-            <button class="level-btn" @click='$emit("next")'>&#8680;</button>
+          <button class="level-select-btn">Level select menu</button>
+          <button class="reset-btn">Reset level</button>
+          <button class="level-btn" title="Previous question" @click='$emit("prev")'>&#8678;</button>
+          <button class="level-btn" title="Next question" @click='$emit("next")'>&#8680;</button>
         </div>      
  
       </nav>
