522dd98c2455acc1e64631433047e55f9540e14e
David Johnson <david_karl_johnson@hotmail.com>
2025-10-13 18:18:08 +0200

    Team04#101: Create "blinking" animation for next button
    The 'Next question' button will now blink in green when the user
    has made a guess. Indicating where they should click to move forwards.

--- team04/components/navigation.js
+++ team04/components/navigation.js
@@ -1,14 +1,31 @@
 export default {
     name: "Navigation",
+    props: {
+      selectedOption: {
+        type: Number,
+        default: null
+      }
+    },
     template: `
       <nav class="navigation">
-          <button class="level-select-btn">Level select menu</button>
-          <button class="reset-btn">Reset level</button>
+          <button class="level-select-btn" title="Return to level selection menu">Level select menu</button>
+          <button class="reset-btn" title="Reset the current game session">Reset session</button>
           <button class="level-btn" title="Previous question" @click='$emit("prev")'>&#8678;</button>
-          <button class="level-btn" title="Next question" @click='$emit("next")'>&#8680;</button>
+          <button class="level-btn" title="Next question" @click='$emit("next")' :style="highlightNextQuestionBtn">&#8680;</button>
         </div>      
  
       </nav>
-
-    `
+    `,
+    computed: {
+    /**
+     * Create a "blinking effect" by changing the background-color of
+     * the Next button when user has made an answer
+     * @returns {CSS} CSS animation propert
+     */
+    highlightNextQuestionBtn() {
+      if (this.selectedOption !== null) {
+        return { animation: 'blinkingEffect 2s infinite' }
+      }
+    }
+  }
   };
