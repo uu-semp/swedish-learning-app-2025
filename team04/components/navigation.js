3f9611a9dd4d9f7cace7a9dbfb2b68acb48e913c
David Johnson <david_karl_johnson@hotmail.com>
2025-10-13 17:30:44 +0200

    Change styling of level select buttons. Move buttons within the navigation grouping

--- team04/components/navigation.js
+++ team04/components/navigation.js
@@ -2,14 +2,15 @@ export default {
     name: "Navigation",
     template: `
       <nav class="navigation">
-        <div class="level-select-group">
-            <button class="level-btn" @click='$emit("prev")'>&lt;</button>
-            <button class="level-btn" @click='$emit("next")'>&gt;</button>
-        </div>      
         <div class="navigation-btn-group">
             <button class="main-menu-btn">Main menu</button>
             <button class="reset-btn">Reset level</button>
         </div>
+        <div class="level-select-group">
+            <button class="level-btn" @click='$emit("prev")'>&#8678;</button>
+            <button class="level-btn" @click='$emit("next")'>&#8680;</button>
+        </div>      
+ 
       </nav>
 
     `
