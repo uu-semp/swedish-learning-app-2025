23241942ed23353a5fdb4f7b48a20dd6b5365b1d
Matilda <matilda.stenbaek.1554@student.uu.se>
2025-10-15 19:26:43 +0200

    fix merge

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function renderGrid(games) {
     
     // Build game card HTML
     card.innerHTML = `
-      <img src="assets/main_menu/images/games/${g.id}.png" 
+      <img src="assets/main_menu/images/game_logos/${g.id}_logo.png" 
            alt="${g.title[currentLanguage]}"
            onerror="this.onerror=null; this.src='assets/main_menu/images/games/default_image.png';">
       <div class="card-body">
