b145447b9aab349e4a5ee4b1137b0ae94c083418
Valle <darkfrobozz.mega@gmail.com>
2025-10-16 13:45:16 +0200

    updated to not use db on start page

--- team08/index.js
+++ team08/index.js
@@ -3,28 +3,20 @@
 // ==============================================
 
 import "./options/settings.js";
-import { init_db } from "./store/read.js";
 
-const init = async () => {
-  await init_db();
+const startBtn = document.getElementById("start-btn");
+const tutorialBtn = document.querySelector(".welcome__tutorial-btn");
 
-  const startBtn = document.getElementById("start-btn");
-  const tutorialBtn = document.querySelector(".welcome__tutorial-btn");
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    // Navigate to main game page
+    window.location.href = "./select.html";
+  });
+}
 
-  if (startBtn) {
-    startBtn.addEventListener("click", () => {
-      // Navigate to main game page
-      window.location.href = "./select.html";
-    });
-  }
-
-  if (tutorialBtn) {
-    tutorialBtn.addEventListener("click", () => {
-      // TODO Navigate to tutorial page
-      window.location.href = "./end-screen/index.html";
-    });
-  }
-};
-
-// Initialize when DOM is ready
-document.addEventListener("DOMContentLoaded", init);
+if (tutorialBtn) {
+  tutorialBtn.addEventListener("click", () => {
+    // TODO Navigate to tutorial page
+    window.location.href = "./end-screen/index.html";
+  });
+}
