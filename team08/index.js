63b488f9ef7ffe62282bdf1697975083d8f0b56a
Valle <darkfrobozz.mega@gmail.com>
2025-10-16 16:56:50 +0200

    added a tutorial with some information about how the game works and some information about keybindings

--- team08/index.js
+++ team08/index.js
@@ -5,18 +5,30 @@
 import "./options/settings.js";
 
 const startBtn = document.getElementById("start-btn");
+
+startBtn.addEventListener("click", () => {
+  // Navigate to main game page
+  window.location.href = "./select.html";
+});
+
+/** Handling tutorial */
+
 const tutorialBtn = document.querySelector(".welcome__tutorial-btn");
+/** @type {HTMLDialogElement | null} */
+const tutorial_modal = document.getElementById("tutorial-modal");
 
-if (startBtn) {
-  startBtn.addEventListener("click", () => {
-    // Navigate to main game page
-    window.location.href = "./select.html";
-  });
-}
+const close_tutorial_modal = document.getElementById("close-tutorial");
 
-if (tutorialBtn) {
-  tutorialBtn.addEventListener("click", () => {
-    // TODO Navigate to tutorial page
-    window.location.href = "./end-screen/index.html";
-  });
-}
+tutorialBtn.addEventListener("click", () => {
+  tutorial_modal?.showModal();
+});
+
+close_tutorial_modal.addEventListener("click", () => {
+  tutorial_modal?.close();
+});
+
+tutorial_modal?.addEventListener("click", (e) => {
+  if (e.target === tutorial_modal) {
+    tutorial_modal.close();
+  }
+});
