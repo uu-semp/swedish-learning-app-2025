7a5479e68a74bf5c4a7279058aa1ec2cc68c9b86
Haoran Wang <Haoran.wang1103@gmail.com>
2026-09-16 17:38:18 +0200

    Created mode 2 where there  are only english words on shopping list and only swedish words on shelf.  And  also make the shopping list more visually clear so that players know where they are, and exclude some inappropriate words.

--- game11/data.js
+++ game11/data.js
@@ -1,13 +1,20 @@
 // Returns an array with all ids for this teams items.
 export function getItemsIds() {
+  // Exclude some ids that are not food/fruits that you can buy in a grocery store
+  // which is : breakfast, dinner, food, fruit, lunch, vegan, vegetarian
+  const excludedIds = ["567f323c", "2f373051","32191560", "440d3157", "75387a51", "19263071", "6a701276"];
   const Ids = [];
   const foods = window.vocabulary.get_category("food") || [];
   const fruits = window.vocabulary.get_category("fruit") || [];
   foods.map(id => {
-    Ids.push(id);
+    if (!excludedIds.includes(id)) {
+          Ids.push(id);
+      }
   })
   fruits.map(id => {
-    Ids.push(id);
+    if (!excludedIds.includes(id)) {
+          Ids.push(id);
+      }
   })
   return Ids;
 }
