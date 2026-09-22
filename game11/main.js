0fd73128cb9c003c3311dfd304d9aeebfc68af8e
Alvin Ljung Hallgren <vezon@live.se>
2026-09-22 15:21:20 +0200

    Added the ability for the user to see the licence of the pictures in the shelf

--- game11/main.js
+++ game11/main.js
@@ -1,5 +1,5 @@
 import { initGameState } from "./state.js";
-import { displayShelf, displayShoppingList } from "./ui.js";
+import { displayShelf, displayShoppingList, displayCopyright} from "./ui.js";
 
 let gameState = {};
 function startGame() {
@@ -12,6 +12,7 @@ function startGame() {
     console.log(gameState.shelf);
     displayShelf(gameState.shelf, gameState.mode);
     displayShoppingList(gameState.shoppingList, gameState.mode);
+    displayCopyright(gameState.shelf, gameState.mode);
   });
   
   window.__game11GameState = gameState;
